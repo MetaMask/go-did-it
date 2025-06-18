807dd553df691d05ddecaf2b28568177041b8da2
Michael Muré <batolettre@gmail.com>
2025-06-18 12:11:52 +0200

    ed25519/x25519: simplify key implementation, add tests

--- verifications/ed25519/key.go
+++ verifications/ed25519/key.go
@@ -5,8 +5,7 @@ import (
 	"crypto/rand"
 	"fmt"
 
-	mbase "github.com/multiformats/go-multibase"
-	"github.com/multiformats/go-varint"
+	"github.com/INFURA/go-did/verifications/internal"
 )
 
 type PublicKey = ed25519.PublicKey
@@ -25,7 +24,7 @@ func GenerateKeyPair() (PublicKey, PrivateKey, error) {
 	return ed25519.GenerateKey(rand.Reader)
 }
 
-// PublicKeyFromBytes convert a serialized public key to a PublicKey.
+// PublicKeyFromBytes converts a serialized public key to a PublicKey.
 // It errors if the slice is not the right size.
 func PublicKeyFromBytes(b []byte) (PublicKey, error) {
 	if len(b) != PublicKeySize {
@@ -36,38 +35,25 @@ func PublicKeyFromBytes(b []byte) (PublicKey, error) {
 
 // PublicKeyFromMultibase decodes the public key from its Multibase form
 func PublicKeyFromMultibase(multibase string) (PublicKey, error) {
-	baseCodec, bytes, err := mbase.Decode(multibase)
-	if err != nil {
-		return nil, err
-	}
-	// the specification enforces that encoding
-	if baseCodec != mbase.Base58BTC {
-		return nil, fmt.Errorf("not Base58BTC encoded")
-	}
-	code, read, err := varint.FromUvarint(bytes)
+	code, bytes, err := helpers.MultibaseDecode(multibase)
 	if err != nil {
 		return nil, err
 	}
 	if code != MultibaseCode {
 		return nil, fmt.Errorf("invalid code")
 	}
-	if read != 2 {
-		return nil, fmt.Errorf("unexpected multibase")
-	}
-	if len(bytes)-read != PublicKeySize {
+	if len(bytes) != PublicKeySize {
 		return nil, fmt.Errorf("invalid ed25519 public key size")
 	}
-	return bytes[read:], nil
+	return bytes, nil
 }
 
 // PublicKeyToMultibase encodes the public key in a suitable way for publicKeyMultibase
 func PublicKeyToMultibase(pub PublicKey) string {
-	// can only fail with an invalid encoding, but it's hardcoded
-	bytes, _ := mbase.Encode(mbase.Base58BTC, append(varint.ToUvarint(MultibaseCode), pub...))
-	return bytes
+	return helpers.MultibaseEncode(MultibaseCode, pub)
 }
 
-// PrivateKeyFromBytes convert a serialized public key to a PrivateKey.
+// PrivateKeyFromBytes converts a serialized public key to a PrivateKey.
 // It errors if the slice is not the right size.
 func PrivateKeyFromBytes(b []byte) (PrivateKey, error) {
 	if len(b) != ed25519.PrivateKeySize {
